2978eb0ac34434c8edbe4f94a525441b99a96fc1
Shogun <shogunwarlord@protonmail.com>
2022-02-26 13:49:07 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+document.getElementById("open-modal").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "block";
+})
+
+
+document.getElementById("close-modal").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none";
+})
